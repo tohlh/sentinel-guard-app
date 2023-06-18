7d6649eb82d277d26fd2bee4703bff70be319a0f
tohlh <tohliheng@gmail.com>
2023-06-19 00:38:59 +0800

    Removed unused loggedIn method

--- lib/src/auth/auth_service.dart
+++ lib/src/auth/auth_service.dart
@@ -1,16 +1,6 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthService {
-  static Future<bool> loggedIn() async {
-    const storage = FlutterSecureStorage();
-    final token = await storage.read(key: 'jwt') ?? '';
-    if (token.isNotEmpty) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
   static Future<String> getToken() async {
     const storage = FlutterSecureStorage();
     return await storage.read(key: 'jwt') ?? '';
