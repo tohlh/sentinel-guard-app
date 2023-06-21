654bb53749eeea6d32b464c6f783bbbe4606c13f
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-06-22 00:19:23 +0800

    Add Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         // '/home': (context) => routeGuard(const HomePage(), const LoginPage()),
-        '/home': (context) => const HomePage(),
+        '/home': (context) => const Home(),
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
       },
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,20 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:sentinel_guard_app/src/api/user_api_service.dart';
 import 'package:sentinel_guard_app/src/models/user.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _HomePageState extends State<HomePage> {
-  late Future<User> futureUser;
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text("Home", style: optionStyle,),
+    HomePage(),
+  ];
 
-  @override
-  void initState() {
-    super.initState();
-    futureUser = UserApiService.getUserProfile();
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -25,6 +31,48 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('SentinelGuard Home Page'),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          )
+        ],
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+      ),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex),)
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<User> futureUser;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // const Duration(seconds: 2);
+    futureUser = UserApiService.getUserProfile();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
       body: Container(
         alignment: Alignment.center,
         padding: const EdgeInsets.all(32),
@@ -34,6 +82,7 @@ class _HomePageState extends State<HomePage> {
               future: futureUser,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
+                  print(snapshot.data!.name);
                   return Text(snapshot.data!.name);
                 } else if (snapshot.hasError) {
                   return Text('${snapshot.error}');
